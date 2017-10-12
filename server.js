e7eda7fee38f9a4492d68ba2563c82cfd0062a2d
System <none>
2017-10-12 18:28:07 +0000

    null

---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/*app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});*/
+
+app.get("/api/whoami", (req, res) => {
+  var iq = req.headers['x-forwarded-for'].split(',')[0] || req.connection.remoteAddress;
+  var lang = req.headers["accept-language"].split(',')[0];
+  var software = req.headers['user-agent'].match(/\(([^()]*)\)/)[1];
+  
+  console.log(req.headers["accept-language"].split(',')[0]);
+  console.log(req.headers['user-agent'].match(/\(([^()]*)\)/)[1]);
+  console.log(req.headers['x-forwarded-for'].split(',')[0]);
+  
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/*
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+*/
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
